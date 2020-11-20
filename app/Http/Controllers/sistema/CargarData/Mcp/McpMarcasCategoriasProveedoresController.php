7d52ffab1fa845019dbd254907ed745b84fca4e3
YefrixD <gerson.vilca@tecsup.edu.pe>
2020-11-20 18:30:02 -0500

    comprobar si no existe campo en el excel

--- app/Http/Controllers/sistema/CargarData/Mcp/McpMarcasCategoriasProveedoresController.php
+++ app/Http/Controllers/sistema/CargarData/Mcp/McpMarcasCategoriasProveedoresController.php
@@ -39,172 +39,176 @@ class McpMarcasCategoriasProveedoresController extends Controller
             $productosku = $objPHPExcel->getActiveSheet()->getCell('F'.$i)->getCalculatedValue();
             $producto    = $objPHPExcel->getActiveSheet()->getCell('G'.$i)->getCalculatedValue();
 
+            if($categoria){
+                $prv = prvproveedores::where('prvnombre', $proveedor)->first(['prvid']);
+                $prvid = 0;
+                if(!$prv){
+                    $prvn = new prvproveedores;
+                    $prvn->prvnombre    = $proveedor;
+                    $prvn->prvprincipal = false;
+                    $prvn->save();
 
-            $prv = prvproveedores::where('prvnombre', $proveedor)->first(['prvid']);
-            $prvid = 0;
-            if(!$prv){
-                $prvn = new prvproveedores;
-                $prvn->prvnombre    = $proveedor;
-                $prvn->prvprincipal = false;
-                $prvn->save();
-
-                $prvid = $prvn->prvid;
-            }else{
-                $prvid = $prv->prvid;
-            }
-
-            $mar = marmarcas::where('marnombre', $marca)->first(['marid']);
-            $marid = 0;
-            if(!$mar){
-                $marn = new marmarcas;
-                $marn->marnombre = $marca;
-                $marn->save();
-                
-                $marid = $marn->marid;
-            }else{
-                $marid = $mar->marid;
-            }
-
-            $catid = 0;
-            if($categoria == "PHIGIENICO"){
-                $catid = 1;
-            }else if($categoria == "FACIALES"){
-                $catid = 6;
-            }else if($categoria == "JABONES"){
-                $catid = 5;
-            }else if($categoria == "MASCARILLAS"){
-                $catid = 7;
-            }else if($categoria == "PAÑOS"){
-                $catid = 4;
-            }else if($categoria == "PTOALLA"){
-                $catid = 2;
-            }else if($categoria == "SERVILLETAS"){
-                $catid = 3;
-            }
-
-            $pro = proproductos::where('prosku', $productosku)
-                                ->first(['proid']);
-                                
-            $proid = 0;
-            if($pro){
-                $pro->marid = $marid;
-                $pro->update();
-
-                $proid = $pro->proid;
-            }else{
-                $pron = new proproductos;
-                $pron->catid     = $catid;
-                $pron->marid     = $marid;
-                $pron->pronombre = $producto;
-                $pron->prosku    = $productosku;
-                if($pron->save()){
-                    $proid = $pron->proid;
+                    $prvid = $prvn->prvid;
                 }else{
-                    $logs[] = "No se pudo agregar el producto: ".$producto." con codigo: ".$productosku;
+                    $prvid = $prv->prvid;
                 }
 
-            }
-
-            $cap = capcategoriasproveedores::where('prvid', $prvid)
-                                    ->where('catid', $catid)
-                                    ->first(['capid']);
-            $capid = 0;
-            if(!$cap){
-                $capn = new capcategoriasproveedores;
-                $capn->prvid = $prvid;
-                $capn->catid = $catid;
-                $capn->save();
-
-                $capid = $capn->capid;
-            }else{
-                $capid = $cap->capid;
-            }
-
-            $map = mapmarcasproveedores::where('prvid', $prvid)
-                                ->where('marid', $marid)
-                                ->first(['mapid']);
-            
-            $mapid = 0;
-            if(!$map){
-                $mapn = new mapmarcasproveedores;
-                $mapn->prvid = $prvid;
-                $mapn->marid = $marid;
-                $mapn->save();
-
-                $mapid = $mapn->mapid;
-            }else{
-                $mapid = $map->mapid;
-            }
-
-
-            $mcp = mcpmarcascategoriasproveedores::where('capid', $capid)
-                                                ->where('catid', $catid)
-                                                ->where('mapid', $mapid)
-                                                ->where('marid', $marid)
-                                                ->where('prvid', $prvid)
-                                                ->first(['mcpid']);
-
-            $mcpid = 0;
-            if(!$mcp){
-                $mcpn = new mcpmarcascategoriasproveedores;
-                $mcpn->capid = $capid;
-                $mcpn->catid = $catid;
-                $mcpn->mapid = $mapid;
-                $mcpn->marid = $marid;
-                $mcpn->prvid = $prvid;
-                $mcpn->save();
-            }else{
-                $logs[] = "El mcp ya existe: ".$mcp->mcpid;
-            }
-
-
-            // 
-            $fechacreacion = new \DateTime(date("Y-m-d H:i:s", strtotime('2020-11-18')));
-            $fechaactualizacion = new \DateTime(date("Y-m-d H:i:s", strtotime('2020-11-18')));
-
-            $fec = fecfechas::where('fecfecha', $fechacreacion)->first(['fecid']);
-            $fecid = 0;
-            if($fec){
-                $fecid = $fec->fecid;
-            }else{
-                $fecn = new fecfechas;
-                $fecn->fecfecha = $fechacreacion;
-                if($fecn->save()){
-                    $fecid = $fecn->fecid;
+                $mar = marmarcas::where('marnombre', $marca)->first(['marid']);
+                $marid = 0;
+                if(!$mar){
+                    $marn = new marmarcas;
+                    $marn->marnombre = $marca;
+                    $marn->save();
+                    
+                    $marid = $marn->marid;
                 }else{
-                    $logs[] = "No se pudo agregar la fecha";
+                    $marid = $mar->marid;
                 }
-            }
 
-            $prs = prsproductossucursales::where('prvid', $prvid)
-                                        ->where('proid', $proid)
+                $catid = 0;
+                if($categoria == "PHIGIENICO"){
+                    $catid = 1;
+                }else if($categoria == "FACIALES"){
+                    $catid = 6;
+                }else if($categoria == "JABONES"){
+                    $catid = 5;
+                }else if($categoria == "MASCARILLAS"){
+                    $catid = 7;
+                }else if($categoria == "PAÑOS"){
+                    $catid = 4;
+                }else if($categoria == "PTOALLA"){
+                    $catid = 2;
+                }else if($categoria == "SERVILLETAS"){
+                    $catid = 3;
+                }
+
+                $pro = proproductos::where('prosku', $productosku)
+                                    ->first(['proid']);
+                                    
+                $proid = 0;
+                if($pro){
+                    $pro->marid = $marid;
+                    $pro->update();
+
+                    $proid = $pro->proid;
+                }else{
+                    $pron = new proproductos;
+                    $pron->catid     = $catid;
+                    $pron->marid     = $marid;
+                    $pron->pronombre = $producto;
+                    $pron->prosku    = $productosku;
+                    if($pron->save()){
+                        $proid = $pron->proid;
+                    }else{
+                        $logs[] = "No se pudo agregar el producto: ".$producto." con codigo: ".$productosku;
+                    }
+
+                }
+
+                $cap = capcategoriasproveedores::where('prvid', $prvid)
                                         ->where('catid', $catid)
-                                        ->where('marid', $marid)
-                                        ->first(['prsid']);
+                                        ->first(['capid']);
+                $capid = 0;
+                if(!$cap){
+                    $capn = new capcategoriasproveedores;
+                    $capn->prvid = $prvid;
+                    $capn->catid = $catid;
+                    $capn->save();
 
-            $prsid = 0;
-
-            if(!$prs){
-                $prsn = new prsproductossucursales;
-                $prsn->prvid      = $prvid;
-                $prsn->proid      = $proid;
-                $prsn->catid      = $catid;
-                $prsn->marid      = $marid;
-                $prsn->fecid      = $fecid;
-                $prsn->prsstock   = 0;
-                $prsn->created_at = $fechacreacion;
-                $prsn->updated_at = $fechaactualizacion;
-                if($prsn->save()){
-                    $prsid = $prsn->prsid;
-                    $rps = new rpsregistroproductossucursales;
-                    $rps->prsid    = $prsid;
-                    $rps->fecid    = $fecid;
-                    $rps->rpsstock = 0;
-                    $rps->save();
+                    $capid = $capn->capid;
                 }else{
-                    $logs[] = "No se pudo agregar el prs";
+                    $capid = $cap->capid;
                 }
+
+                $map = mapmarcasproveedores::where('prvid', $prvid)
+                                    ->where('marid', $marid)
+                                    ->first(['mapid']);
+                
+                $mapid = 0;
+                if(!$map){
+                    $mapn = new mapmarcasproveedores;
+                    $mapn->prvid = $prvid;
+                    $mapn->marid = $marid;
+                    $mapn->save();
+
+                    $mapid = $mapn->mapid;
+                }else{
+                    $mapid = $map->mapid;
+                }
+
+
+                $mcp = mcpmarcascategoriasproveedores::where('capid', $capid)
+                                                    ->where('catid', $catid)
+                                                    ->where('mapid', $mapid)
+                                                    ->where('marid', $marid)
+                                                    ->where('prvid', $prvid)
+                                                    ->first(['mcpid']);
+
+                $mcpid = 0;
+                if(!$mcp){
+                    $mcpn = new mcpmarcascategoriasproveedores;
+                    $mcpn->capid = $capid;
+                    $mcpn->catid = $catid;
+                    $mcpn->mapid = $mapid;
+                    $mcpn->marid = $marid;
+                    $mcpn->prvid = $prvid;
+                    $mcpn->save();
+                }else{
+                    $logs[] = "El mcp ya existe: ".$mcp->mcpid;
+                }
+
+
+                // 
+                $fechacreacion = new \DateTime(date("Y-m-d H:i:s", strtotime('2020-11-18')));
+                $fechaactualizacion = new \DateTime(date("Y-m-d H:i:s", strtotime('2020-11-18')));
+
+                $fec = fecfechas::where('fecfecha', $fechacreacion)->first(['fecid']);
+                $fecid = 0;
+                if($fec){
+                    $fecid = $fec->fecid;
+                }else{
+                    $fecn = new fecfechas;
+                    $fecn->fecfecha = $fechacreacion;
+                    if($fecn->save()){
+                        $fecid = $fecn->fecid;
+                    }else{
+                        $logs[] = "No se pudo agregar la fecha";
+                    }
+                }
+
+                $prs = prsproductossucursales::where('prvid', $prvid)
+                                            ->where('proid', $proid)
+                                            ->where('catid', $catid)
+                                            ->where('marid', $marid)
+                                            ->first(['prsid']);
+
+                $prsid = 0;
+
+                if(!$prs){
+                    $prsn = new prsproductossucursales;
+                    $prsn->prvid      = $prvid;
+                    $prsn->proid      = $proid;
+                    $prsn->catid      = $catid;
+                    $prsn->marid      = $marid;
+                    $prsn->fecid      = $fecid;
+                    $prsn->prsstock   = 0;
+                    $prsn->created_at = $fechacreacion;
+                    $prsn->updated_at = $fechaactualizacion;
+                    if($prsn->save()){
+                        $prsid = $prsn->prsid;
+                        $rps = new rpsregistroproductossucursales;
+                        $rps->prsid    = $prsid;
+                        $rps->fecid    = $fecid;
+                        $rps->rpsstock = 0;
+                        $rps->save();
+                    }else{
+                        $logs[] = "No se pudo agregar el prs";
+                    }
+                }
+            }else{
+
             }
+            
 
 
 
