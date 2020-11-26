dfc340f135fc2fbee591ad0a12871314270a7522
YefrixD <gerson.vilca@tecsup.edu.pe>
2020-11-26 18:11:30 -0500

    mcps para proveedor

--- app/Http/Controllers/sistema/pruebaController.php
+++ app/Http/Controllers/sistema/pruebaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\sistema;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\mcpmarcascategoriasproveedores;
 
 class pruebaController extends Controller
 {
