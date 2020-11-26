0507da4b55f3636ba4a66e518cde82e4d16e0273
YefrixD <gerson.vilca@tecsup.edu.pe>
2020-11-26 18:24:08 -0500

    mcps para proveedor

--- app/Http/Controllers/sistema/pruebaController.php
+++ app/Http/Controllers/sistema/pruebaController.php
@@ -10,26 +10,27 @@ class pruebaController extends Controller
 {
     public function ProveedoresSkus()
     {
-        $mcps = mcpmarcascategoriasproveedores::join('prvproveedores as prv', 'prv.prvid', 'mcpmarcascategoriasproveedores.prvid')
-                                                ->join('catcategorias as cat', 'cat.catid', 'mcpmarcascategoriasproveedores.catid')
-                                                ->join('proproductos as pro', 'pro.catid', 'cat.catid')
-                                                ->distinct('prv.prvid')
-                                                ->get([
-                                                    'prv.prvnombre',
-                                                    'cat.catnombre',
-                                                    'pro.prosku',
-                                                    'pro.pronombre'
-                                                ]);
-
         // $mcps = mcpmarcascategoriasproveedores::join('prvproveedores as prv', 'prv.prvid', 'mcpmarcascategoriasproveedores.prvid')
-        //                                         ->join('marmarcas as mar', 'mar.marid', 'mcpmarcascategoriasproveedores.marid')
-        //                                         ->join('proproductos as pro', 'pro.marid', 'mar.marid')
+        //                                         ->join('catcategorias as cat', 'cat.catid', 'mcpmarcascategoriasproveedores.catid')
+        //                                         ->join('proproductos as pro', 'pro.catid', 'cat.catid')
+        //                                         ->distinct('prv.prvid')
         //                                         ->get([
         //                                             'prv.prvnombre',
-        //                                             'mar.marnombre',
+        //                                             'cat.catnombre',
         //                                             'pro.prosku',
         //                                             'pro.pronombre'
         //                                         ]);
+
+        $mcps = mcpmarcascategoriasproveedores::join('prvproveedores as prv', 'prv.prvid', 'mcpmarcascategoriasproveedores.prvid')
+                                                ->join('marmarcas as mar', 'mar.marid', 'mcpmarcascategoriasproveedores.marid')
+                                                ->join('proproductos as pro', 'pro.marid', 'mar.marid')
+                                                ->distinct('prv.prvid')
+                                                ->get([
+                                                    'prv.prvnombre',
+                                                    'mar.marnombre',
+                                                    'pro.prosku',
+                                                    'pro.pronombre'
+                                                ]);
 
         return response()->json([
             'datos' => $mcps
