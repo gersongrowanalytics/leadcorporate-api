102f117184006b0d71db115f4dceca69b71c015d
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2020-12-04 10:53:40 -0500

    stock 3 12

--- app/Http/Controllers/sistema/CargarData/Pro/StockProductosController.php
+++ app/Http/Controllers/sistema/CargarData/Pro/StockProductosController.php
@@ -131,7 +131,8 @@ class StockProductosController extends Controller
         // $fichero_subido = base_path().'/public/sistema/excels/stocks/AL_29_11_I.xlsx';
         // $fichero_subido = base_path().'/public/sistema/excels/stocks/AL_30_11_I.xlsx';
         // $fichero_subido = base_path().'/public/sistema/excels/stocks/AL_01_12_I.xlsx';
-        $fichero_subido = base_path().'/public/sistema/excels/stocks/AL_02_12_I.xlsx';
+        // $fichero_subido = base_path().'/public/sistema/excels/stocks/AL_02_12_I.xlsx';
+        $fichero_subido = base_path().'/public/sistema/excels/stocks/AL_03_12_I.xlsx';
 
         $objPHPExcel    = IOFactory::load($fichero_subido);
         $objPHPExcel->setActiveSheetIndex(0);
